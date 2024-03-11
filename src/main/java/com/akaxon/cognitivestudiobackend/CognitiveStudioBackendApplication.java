5e1c622efd79c1bcff59b0890f507d13873e5bb6
xasan tuychiyev <ustozshifu001@gmail.com>
2024-03-11 10:58:47 +0500

    first commit

---
+++ src/main/java/com/akaxon/cognitivestudiobackend/CognitiveStudioBackendApplication.java
@@ -0,0 +1,13 @@
+package com.akaxon.cognitivestudiobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CognitiveStudioBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CognitiveStudioBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/akaxon/cognitivestudiobackend/CognitiveStudioBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akaxon.cognitivestudiobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CognitiveStudioBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
